2121b81e4e26217e75e3cdd8007ea4d7322c75e1
Iván SZKIBA <szkiba@users.noreply.github.com>
2020-01-20 19:04:49 +0100

    add link rewrite to init hook

--- src/index.js
+++ src/index.js
@@ -100,6 +100,8 @@ $docsify.plugins = [].concat(function (hook, vm) {
                 window.$docsify.logo = resolve(window.$docsify.logo);
             }
         }
+
+        rewriteLinks();
     });
 
     function cloudInit() {
@@ -200,6 +202,16 @@ $docsify.plugins = [].concat(function (hook, vm) {
         }
 
         rewriteLinks();
+    });
+
+    hook.beforeEach(function (content) {
+        rewriteLinks();
+        return content;
+    });
+
+    hook.afterEach(function (html, next) {
+        rewriteLinks();
+        next(html);
     });
 
     hook.doneEach(rewriteLinks);
